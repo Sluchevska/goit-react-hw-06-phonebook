016ebe5c488eee5bad0d7c29d81bc6647338bc95
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-03 20:04:12 +0100

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import contactReducer from "../redux/contacts-reducer";
+// /* eslint-disable import/no-anonymous-default-export */
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer, FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import contactReducer from "../redux/contacts-reducer";
 
-const contactsPersistConfig = {
-    key: 'contacts',
-    storage,
-    blacklist: ['filter'],
-};
+// const contactsPersistConfig = {
+//     key: 'contacts',
+//     storage,
+//     blacklist: ['filter'],
+// };
+
+// const store = configureStore({
+//     reducer: {
+//     contacts: persistReducer(contactsPersistConfig, contactReducer),
+// },
+//     devTools: process.env.NODE_ENV !== 'production',
+//     middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// const persistor = persistStore(store);
+
+// export default {store, persistor};
+
+import { configureStore } from "@reduxjs/toolkit";
+import contactsReducer from "./contacts-reducer";
+
 
 const store = configureStore({
-    reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactReducer),
-},
-    devTools: process.env.NODE_ENV !== 'production',
-    middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+     reducer: {
+          contacts: contactsReducer
+     },
+     devTools: process.env.Node_ENV==='development'
 
-const persistor = persistStore(store);
-
-export default {store, persistor};
+})
+export default store
