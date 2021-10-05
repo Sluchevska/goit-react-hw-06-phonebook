a68f3fd9a4198aec57fa49963b0486fa4fe48893
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-05 21:26:10 +0100

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,24 +2,36 @@ import PropTypes from 'prop-types';
 import { Input, LabelInput } from '../ContactForm/ContactForm.styled';
 import { connect } from 'react-redux';
 import contactsActions from '../../redux/contacts-actions';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-const Filter = ({ value, onChange, onBlur }) => (
-  <label>
-    <LabelInput>Find contact by name</LabelInput>
-    <Input type="text" value={value} onChange={onChange} onBlur={onBlur} />
-  </label>
-);
+export default function Filter() {
+  const value = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+  return (
+    <label>
+      <LabelInput>Find contact by name</LabelInput>
+      <Input
+        type="text"
+        value={value}
+        onChange={e => dispatch(contactsActions.changeFilter(e.target.value))}
+        onBlur={e =>
+          dispatch(contactsActions.changeFilter(e.target.value = ''))
+        }
+      />
+    </label>
+  );
+}
+// const mapStateToProps = state => ({
+//   value: state.contacts.filter,
+// });
 
-const mapStateToProps = state => ({
-  value: state.contacts.filter,
-});
+// const mapDispatchToProps = dispatch => ({
+//   onChange: e => dispatch(contactsActions.changeFilter(e.target.value)),
+//   onBlur: e => dispatch(contactsActions.changeFilter((e.target.value = ''))),
+// });
 
-const mapDispatchToProps = dispatch => ({
-  onChange: e => dispatch(contactsActions.changeFilter(e.target.value)),
-  onBlur: e => dispatch(contactsActions.changeFilter((e.target.value = ''))),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+// export default connect(mapStateToProps, mapDispatchToProps)(Filter);
 
 Filter.propTypes = {
   value: PropTypes.string,
