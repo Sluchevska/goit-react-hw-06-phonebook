c96e31f104422b628f6d7a7d3f4aba58bd1d68b2
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-05 21:55:21 +0100

    update

--- src/redux/contacts-selectors.js
+++ src/redux/contacts-selectors.js
@@ -1,6 +1,9 @@
+export const getContacts = state => state.contacts.items
+export const getFilter = state=> state.contacts.filter
+
 export const getVisibleContacts = state => {
-  const allContacts = state.contacts.items
-  const filter = state.contacts.filter
+  const allContacts = getContacts(state)
+  const filter = getFilter(state)
     const normalizedFilter = filter.toLowerCase();
 
     return allContacts.filter(({ name }) =>
