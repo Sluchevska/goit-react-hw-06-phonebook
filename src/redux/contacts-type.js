a63ced2124e0b3eac055110d2f4eb9395a38fea5
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-05 20:40:38 +0100

    update

--- src/redux/contacts-type.js
+++
@@ -1,7 +0,0 @@
-const ADD = 'contacts/addContact';
-const DELETE = 'contacts/deleteContact';
-const CHANGE_FILTER = 'contacts/changeFilter';
-const BLUR = 'contacts/onBlur';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { ADD, DELETE, CHANGE_FILTER, BLUR };
